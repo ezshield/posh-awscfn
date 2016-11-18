a1267b8dba6c2293fd3bee8460a23959f81dbfa5
Eugene Bekker <ebekker@powervision.com>
2016-11-18 16:41:48 -0500

    Enahancing the DIFF test
    Printing the differences

--- AwsCfn/gen/Test-SchemaUpdated.ps1
+++ AwsCfn/gen/Test-SchemaUpdated.ps1
@@ -1,7 +1,7 @@
 ﻿
 $cfnSchemaUrl = 'http://vstoolkit.amazonwebservices.com/CloudFormationSchema/CloudFormationV1.schema'
 $schema = "$PSScriptRoot\CloudFormationV1.schema"
-
+ren $schema "$schema.OLD"
 
 Invoke-WebRequest $cfnSchemaUrl -OutFile $schema
 $oldHash = Get-Content "$schema.sha256"
@@ -9,9 +9,14 @@ $newHash = .\Compute-SchemaHash.ps1
 
 if ($oldHash -ne $newHash) {
     $msg = "CloudFormation Schema has been UPDATED!"
+    Write-Host $msg
+    Write-Host "Here are the changes..."
+    Write-Host "*******************************************************************"
+    fc.exe "$schema.OLD" $schema
 }
 else {
     $msg = "Cloudormation Schema is still the same"
+    Write-Host $msg
 }
 
 $slackWebHook = $env:SLACK_WEBHOOK
