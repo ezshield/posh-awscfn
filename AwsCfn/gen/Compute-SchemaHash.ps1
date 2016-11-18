6bcaab6457d95b9743d547b43da5ba8b7c5b25ea
Eugene Bekker <ebekker@powervision.com>
2016-11-18 16:14:51 -0500

    Adding support for detecting changes in CFN schema
    To be run nightly by AppVeyor project and send out notifications

---
+++ AwsCfn/gen/Compute-SchemaHash.ps1
@@ -0,0 +1,8 @@
+﻿$schema = "$PSScriptRoot\CloudFormationV1.schema"
+$bytes = [System.IO.File]::ReadAllBytes($schema)
+$sha = [System.Security.Cryptography.SHA256]::Create()
+$hash = $sha.ComputeHash($bytes)
+$hashStr = [System.BitConverter]::ToString($hash).Replace('-', '')
+
+$hashStr
+
---
+++ AwsCfn/gen/Test-SchemaUpdated.ps1
@@ -0,0 +1,18 @@
+﻿
+$cfnSchemaUrl = 'http://vstoolkit.amazonwebservices.com/CloudFormationSchema/CloudFormationV1.schema'
+$schema = "$PSScriptRoot\CloudFormationV1.schema"
+
+
+Invoke-WebRequest $cfnSchemaUrl -OutFile $schema
+$oldHash = Get-Content "$schema.sha256"
+$newHash = .\Compute-SchemaHash.ps1
+
+if ($oldHash -ne $newHash) {
+    $msg = "CloudFormation Schema has been UPDATED!"
+}
+else {
+    $msg = "Cloudormation Schema is still the same"
+}
+
+$slackWebHook = 'https://hooks.slack.com/services/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'
+Invoke-WebRequest -Uri $slackWebHook -Method Post -Body "payload={`"text`": `"$msg`" }"
